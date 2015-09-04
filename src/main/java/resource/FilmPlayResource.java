b44430dfb98aab3581614cd83eb8b45027b0c7ca
821600682@qq.com <821600682@qq.com>
2015-09-04 12:39:12 +0800

    youku播放地址代码中，添加自动播放、去除广告的参数，参数形式不是正常的http url格式，目前还不知道规律，中知道，全部用【&isAutoPlay=true&showAd=0】能解决大部分的情况。

--- src/main/java/resource/FilmPlayResource.java
+++ src/main/java/resource/FilmPlayResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.jsoup.Jsoup;
 
 import render.PlayFilm;
@@ -43,13 +44,37 @@ public class FilmPlayResource {
 			final Map<String, Object> movie = li.get(0);
 			final Map<String, Object> features = (Map<String, Object>)movie.get("features");
 			features.put("flash", Strings.nullToEmpty((String)features.get("flash")));
+			
+			if(Strings.isNullOrEmpty((String)features.get("flash"))){
+				features.put("flash", "");
+			}else{
+				final String flash = (String)features.get("flash");
+				if(flash.indexOf('?') > -1){
+					features.put("flash", flash + "&isAutoPlay=true&showAd=0");
+				}else{
+					features.put("flash", flash + "&isAutoPlay=true&showAd=0");
+				}
+//				final URIBuilder ub = new URIBuilder(flash);
+//				ub.addParameter("isAutoPlay", "true");//isAutoPlay=true&showAd=0
+//				ub.addParameter("showAd", "0");
+//				features.put("flash", ub.build().toString());
+			}
 			final String common = (String)features.get("common");
-			if(!Strings.isNullOrEmpty(common)){
+			if(Strings.isNullOrEmpty(common)){
+				features.put("iframe", "");
+			}else{
 				//<iframe height=498 width=510 src=\"http://player.youku.com/embed/XNTAwNDExOTIw\" frameborder=0 allowfullscreen></iframe>
 				final String iframe = Jsoup.parseBodyFragment(common).select("iframe").attr("src");
-				features.put("iframe", Strings.nullToEmpty(iframe));
-			}else{
-				features.put("iframe", "");
+				if(iframe.indexOf('?') > -1){
+					features.put("iframe", iframe + "&isAutoPlay=true&showAd=0");
+				}else{
+					features.put("iframe", iframe + "&isAutoPlay=true&showAd=0");
+				}
+//				final URIBuilder ub = new URIBuilder(iframe);
+//				ub.addParameter("isAutoPlay", "true");//isAutoPlay=true&showAd=0
+//				ub.addParameter("showAd", "0");
+//				features.put("iframe", ub.build().toString());
+//				features.put("iframe", Strings.nullToEmpty(iframe));
 			}
 			final byte[] bs = PlayFilm.render(movie, type, uuid);
 			return Response.ok(bs).build();
